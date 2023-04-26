853057e8543625844755530176dcd363b4b4ed78
SURAJ0866 <106803883+SURAJ0866@users.noreply.github.com>
2023-04-26 11:34:50 +0530

    Add files via upload

---
+++ ReactBenifits.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function ReactBenifits() {
+  return (
+    <div className='benifits'>
+      <h1>Benifits of Using React..</h1>
+      <h3>components based architecture</h3>
+      <h3>virtual Dom of efficient updates</h3>
+      <h3>Rich Ecosustem and community</h3>
+      <h3>cross platform development</h3>
+      <h3>strong community support</h3>
+    </div>
+  );
+}
+
+export default ReactBenifits;
